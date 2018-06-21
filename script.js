6f3d6364df6e9725a6c4d7a29ab870e98f8e3919
Bryan <bccdp4@gmail.com>
2018-06-21 09:04:49 -0500

    images

--- script.js
+++ script.js
@@ -1,35 +1,19 @@
-$(document).ready(function() {
-	$('kbd').mouseenter(function() {
-		$('kbd').fadeTo('fast', 1);
-	});
-	$('kbd').mouseleave(function() {
-		$('kbd').fadeTo('fast', 0.5);
-	});
-});
-
-var slideIndex = 1;
-showSlides(slideIndex);
-
-function plusSlides(n) {
-  showSlides(slideIndex += n);
-}
-
-function currentSlide(n) {
-  showSlides(slideIndex = n);
-}
-
-function showSlides(n) {
-  var i;
-  var slides = document.getElementsByClassName("slides");
-  var dots = document.getElementsByClassName("dot");
-  if (n > slides.length) {slideIndex = 1} 
-  if (n < 1) {slideIndex = slides.length}
-  for (i = 0; i < slides.length; i++) {
-      slides[i].style.display = "none"; 
-  }
-  for (i = 0; i < dots.length; i++) {
-      dots[i].className = dots[i].className.replace(" active", "");
-  }
-  slides[slideIndex-1].style.display = "block"; 
-  dots[slideIndex-1].className += " active";
+function showhide() {
+	var x = document.getElementById("description");
+	var y = document.getElementById("description2");
+	var z = document.getElementById("description3");
+	if(x.style.display === "none") {
+		x.style.display = "block";
+	}
+	else if(y.style.display === "none") {
+		y.style.display = "block";		
+	}
+	else if(z.style.display === "none") {
+		z.style.display = "block";		
+	}
+	else {
+		x.style.display = "none";
+		y.style.display = "none";
+		z.style.display = "none";
+	}
 }
