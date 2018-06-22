4cae18e77a8aad1362434e0ef00543afdabd775d
Bryan <bccdp4@gmail.com>
2018-06-22 13:44:41 -0500

    email off

--- script.js
+++ script.js
@@ -23,14 +23,17 @@ function openFile() {
 }
 
 function email() {
-	var userName = document.getElementById("contact").elements[0].value;
+	document.getElementById.("error").innerHTML = "Sorry, your email could not be sent at this time.";
+/* 	var userName = document.getElementById("contact").elements[0].value;
 	var userEmail = document.getElementById("contact").elements[1].vlaue;
 	var userMessage = document.getElementById("contact").elements[2].value;	
 	
 	var link = "mailto:bryan.corcoro@gmail.com"
 			+ "?cc=" + userEmail
 			+ "?subject" + escape("Message from: " + userName)
-			+ "&body" + escape(userMessage);
+			+ "&body" + escape(userMessage); */
 			
-	window.location.href = link;
+	// window.location.href = link;
+	
+
 }
