18625af179a5ef94f688cfd5628b78d4846f958a
Bryan <bccdp4@gmail.com>
2018-07-16 10:15:34 -0500

    Project Gallery

--- script.js
+++ script.js
@@ -40,3 +40,22 @@ $(function() {
     });
 });
 
+$('#modal1').on('hidden.bs.modal', function (e) {
+  // do something...
+  $('#modal1 iframe').attr("src", $("#modal1 iframe").attr("src"));
+});
+
+$('#modal6').on('hidden.bs.modal', function (e) {
+  // do something...
+  $('#modal6 iframe').attr("src", $("#modal6 iframe").attr("src"));
+});
+
+$('#modal4').on('hidden.bs.modal', function (e) {
+  // do something...
+  $('#modal4 iframe').attr("src", $("#modal4 iframe").attr("src"));
+});
+
+$('#modal5').on('hidden.bs.modal', function (e) {
+  // do something...
+  $('#modal5 iframe').attr("src", $("#modal5 iframe").attr("src"));
+});
